17506e236718bddb96123a461581f90f79d7e3b2
Brandon Chung <brandonsychung@gmail.com>
2021-12-06 09:57:30 -0500

    Make camera smooth a parameter

--- modules/camera.mjs
+++ modules/camera.mjs
@@ -1,7 +1,8 @@
 class Camera {
-    constructor(pos = { x: 0, y: 0 }, range = {min: 2, max: 4}) {
+    constructor(pos = { x: 0, y: 0 }, range = { min: 2, max: 4 }, smooth = { pos: 0.75, zoom: 0.875 }) {
         this.pos = pos;
         this.range = range;
+        this.smooth = smooth;
 
         this.zoom = range.min;
     }
@@ -13,20 +14,21 @@ class Camera {
         };
 
         if (target.x < this.range.min || target.y < this.range.min) {
-            this.pos.x = 0.75 * this.pos.x + 0.25 * players[0].pos.x;
-            this.pos.y = 0.75 * this.pos.y + 0.25 * players[0].pos.y;
-            this.zoom = 0.875 * this.zoom + 0.125 * this.range.min;
+            this.pos.x = this.smooth.pos * this.pos.x + (1 - this.smooth.pos) * players[0].pos.x;
+            this.pos.y = this.smooth.pos * this.pos.y + (1 - this.smooth.pos) * players[0].pos.y;
+            this.zoom = this.smooth.zoom * this.zoom + (1 - this.smooth.zoom) * this.range.min;
             return;
         }
 
-        this.pos.x = 0.75 * this.pos.x + 0.25 * (players[0].pos.x + players[1].pos.x) / 2;
-        this.pos.y = 0.75 * this.pos.y + 0.25 * (players[0].pos.y + players[1].pos.y) / 2;
+        this.pos.x = this.smooth.pos * this.pos.x + (1 - this.smooth.pos) * (players[0].pos.x + players[1].pos.x) / 2;
+        this.pos.y = this.smooth.pos * this.pos.y + (1 - this.smooth.pos) * (players[0].pos.y + players[1].pos.y) / 2;
 
         target = {
             x: Math.max(Math.min(target.x, this.range.max), this.range.min),
             y: Math.max(Math.min(target.y, this.range.max), this.range.min)
         };
-        this.zoom = 0.875 * this.zoom + 0.125 * Math.min(target.x, target.y);
+        
+        this.zoom = this.smooth.zoom * this.zoom + (1 - this.smooth.zoom) * Math.min(target.x, target.y);
     }
 }
 
