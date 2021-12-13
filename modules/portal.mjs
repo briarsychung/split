f2c511673ca4fe11726343d31ad3206e614f7740
Brandon Chung <brandonsychung@gmail.com>
2021-12-13 06:37:38 -0500

    Add animation to portal

--- modules/portal.mjs
+++ modules/portal.mjs
@@ -1,19 +1,37 @@
 import { Button } from './button.mjs';
+import { Texture } from './texture.mjs';
 
 class Portal extends Button {
-    constructor(pos, dim, texture, offset, destination, denied = []) {
-        super(pos, dim, texture, offset);
+    constructor(range) {
+        let textures =
+            [new Texture('../assets/interactive/portal-inactive.png', { w: 32, h: 32 }),
+            new Texture('../assets/interactive/portal-active1.png', { w: 32, h: 32 }),
+            new Texture('../assets/interactive/portal-active2.png', { w: 32, h: 32 }),
+            new Texture('../assets/interactive/portal-active1.png', { w: 32, h: 32 })];
 
-        this.destination = destination;
-        this.denied = denied;
+        super(range[0], { w: 32, h: 32 }, textures[0], { x: 0, y: -32 });
+
+        this.textures = textures;
+        this.destination = range[1];
+        this.stage = 0;
+    }
+
+    init() {
+        this.stage = 0;
+
+        super.init();
     }
 
     trigger() {
+        this.stage = this.stage === 0 ? 0 : this.stage - 1;
+
         for (let i = 0; i < this.pressed.length; i++) {
-            if (this.denied.includes(this.pressed[i].constructor.name)) continue;
             this.pressed[i].pos.x += this.destination.x - this.pos.x;
             this.pressed[i].pos.y += this.destination.y - this.pos.y;
+            this.stage = 15;
         }
+        
+        this.texture = this.textures[Math.floor(this.stage / 4)];
 
         super.trigger();
     }
